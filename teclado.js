1535c33160520d0dc21abda281f0f5b0f84c07db
hugoresende27 <Hugoresende27@gmail.com>
2021-10-23 18:55:23 +0100

    sab

---
+++ teclado.js
@@ -0,0 +1,17 @@
+const teclado = {       //teclado novo objeto const
+    elementos: {        //objeto elementos 
+        main : null,
+        teclasConteudo:null,
+        teclas:[]       //var arrays teclas
+    },
+    
+    eventos:{
+        oninput:null,
+        onclose:null
+    },
+
+    propriedades: {
+        valor: "",
+        capslock:false
+    }
+}
